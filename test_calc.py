52bb274d1003f59c88fa3ada74bcf43fe510d221
sonneanjali2001-dev <anjalisonni65@gmail.com>
2025-11-11 11:14:14 +0530

    new pipeline

---
+++ test_calc.py
@@ -0,0 +1,15 @@
+import pytest
+from calc import add,sub,mul,div
+
+def test_add():
+    assert add(2,3)==5
+def test_sub():
+    assert sub(5,3)==2
+def test_mul():
+    assert mul(2,3)==6
+def test_div():
+    assert div(6,2)==3
+def test_div_by_zero():
+    with pytest.raises(ValueError):
+     div(5,0)    
+
